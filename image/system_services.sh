b56693beb345781092f294c209cba49ace355dd7
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-05-12 17:29:45 +0200

    Fix syslog-ng.
    The system() source in the default syslog-ng config file does
    not work in Docker because it tries to access /proc/kmsg,
    which is not allowed due to Docker's privileges. We replace
    it by a source that only reads from /dev/log.

--- image/system_services.sh
+++ image/system_services.sh
@@ -21,6 +21,9 @@ mkdir /etc/service/syslog-ng
 cp /build/runit/syslog-ng /etc/service/syslog-ng/run
 mkdir -p /var/lib/syslog-ng
 cp /build/config/syslog_ng_default /etc/default/syslog-ng
+# Replace the system() source because inside Docker we
+# can't access /proc/kmsg.
+sed -i -E 's/^(\s*)system\(\);/\1unix-stream("\/dev\/log");/' /etc/syslog-ng/syslog-ng.conf
 
 ## Install logrotate.
 $minimal_apt_get_install logrotate
