1a6c43b8d7f4201987d413c58d7c72d45f56f8f2
Bernard Potocki <bernard.potocki@imanel.org>
2014-06-02 00:11:24 +0200

    Preserve ENV variables for SSH client
    Currently when you ssh to host you will not see ENV variables set. This was mentioned earlier in #54 and solution is tested.
    
    Additional rationale: currently you can't see docker link ENV variables inside ssh, so you need to load this file every time you connect to container. I would say that expected behavior is to preserve them in services that are provided at default (cron/ssh)

--- image/system_services.sh
+++ image/system_services.sh
@@ -11,6 +11,7 @@ touch /etc/container_environment.sh
 touch /etc/container_environment.json
 chmod 700 /etc/container_environment
 chmod 600 /etc/container_environment.sh /etc/container_environment.json
+ln -s /etc/container_environment.sh /etc/profile.d/
 
 ## Install runit.
 $minimal_apt_get_install runit
