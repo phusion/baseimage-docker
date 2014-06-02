b665b78454b4d4809c90f0583d8491b6868af279
Bernard Potocki <bernard.potocki@imanel.org>
2014-06-02 15:06:02 +0200

    Make container_envoriments.sh and .json readable by other users
    Prevents bug when loading env variables on non-root user login.

--- image/system_services.sh
+++ image/system_services.sh
@@ -10,7 +10,7 @@ mkdir -p /etc/container_environment
 touch /etc/container_environment.sh
 touch /etc/container_environment.json
 chmod 700 /etc/container_environment
-chmod 600 /etc/container_environment.sh /etc/container_environment.json
+chmod 644 /etc/container_environment.sh /etc/container_environment.json
 ln -s /etc/container_environment.sh /etc/profile.d/
 
 ## Install runit.
