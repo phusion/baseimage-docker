101ecb67ba23c5cccafd05811acac189338d8707
David Windell <david@outeredgeuk.com>
2014-10-21 12:46:52 +0100

    Add wget
    It seems to me that if curl is offered as a default then wget should be?

--- image/utilities.sh
+++ image/utilities.sh
@@ -4,7 +4,7 @@ source /build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less nano vim psmisc
+$minimal_apt_get_install curl less nano vim psmisc wget
 
 ## This tool runs a command as another user and sets $HOME.
 cp /build/bin/setuser /sbin/setuser
