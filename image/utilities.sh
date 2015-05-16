24aa494a7973df0e7fc6452f6616615db9d27043
qxo <qxodream@gmail.com>
2015-05-16 14:22:25 +0800

    add wget and busybox(telnet,xz,unzip)

--- image/utilities.sh
+++ image/utilities.sh
@@ -4,7 +4,21 @@ source /build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less nano vim psmisc
+$minimal_apt_get_install wget curl less nano vim psmisc
 
 ## This tool runs a command as another user and sets $HOME.
 cp /build/bin/setuser /sbin/setuser
+
+
+## install busybox for wget and telnet,...
+#/usr/lib/initramfs-tools/bin/busybox  is too old
+#if [ ! -f /usr/lib/initramfs-tools/bin/busybox  ] ; then
+curl  http://www.busybox.net/downloads/binaries/latest/busybox-x86_64 > /usr/local/bin/busybox
+chmod +x /usr/local/bin/busybox
+#else
+#ln -s /usr/lib/initramfs-tools/bin/busybox   /usr/local/bin/busybox
+#fi
+#ln -s $(which busybox) /usr/local/bin/wget
+ln -s $(which busybox) /usr/local/bin/telnet
+ln -s $(which busybox) /usr/local/bin/unzip
+ln -s $(which busybox) /usr/local/bin/xz
