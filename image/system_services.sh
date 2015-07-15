404832ba47b7d7fdc5dba3b858a4ea0dc7e4986b
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-07-15 14:52:39 +0200

    Remove some unnecessary cron jobs
    Closes GH-205.

--- image/system_services.sh
+++ image/system_services.sh
@@ -67,3 +67,8 @@ cp /bd_build/runit/cron /etc/service/cron/run
 ## Remove useless cron entries.
 # Checks for lost+found and scans for mtab.
 rm -f /etc/cron.daily/standard
+rm -f /etc/cron.daily/upstart
+rm -f /etc/cron.daily/dpkg
+rm -f /etc/cron.daily/password
+rm -f /etc/cron.weekly/fstrim
+
