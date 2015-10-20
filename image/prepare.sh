a0ea9a20f3dae7f6251c2ef1cd6768f7af993f07
Jim Riordan <jim@j1mr10rd4n.info>
2015-10-21 08:55:11 +1100

    Don't try to enable ubuntu-specific repositories

--- image/prepare.sh
+++ image/prepare.sh
@@ -16,8 +16,8 @@ mkdir -p /etc/container_environment
 echo -n no > /etc/container_environment/INITRD
 
 ## Enable Ubuntu Universe and Multiverse.
-sed -i 's/^#\s*\(deb.*universe\)$/\1/g' /etc/apt/sources.list
-sed -i 's/^#\s*\(deb.*multiverse\)$/\1/g' /etc/apt/sources.list
+#sed -i 's/^#\s*\(deb.*universe\)$/\1/g' /etc/apt/sources.list
+#sed -i 's/^#\s*\(deb.*multiverse\)$/\1/g' /etc/apt/sources.list
 apt-get update
 
 ## Fix some issues with APT packages.
