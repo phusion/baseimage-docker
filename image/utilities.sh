6d5804701a190c5a506c270795b1c623f3cedba1
Homer Huang <aclisp@gmail.com>
2016-01-07 08:51:01 +0800

    Replace dangerous upstart commands.

--- image/utilities.sh
+++ image/utilities.sh
@@ -9,3 +9,19 @@ ln -s /usr/bin/vim.tiny /usr/bin/vim
 
 ## This tool runs a command as another user and sets $HOME.
 cp /bd_build/bin/setuser /sbin/setuser
+
+## Replace dangerous upstart commands
+rm -f /sbin/init /sbin/telinit /sbin/reboot /sbin/shutdown
+cat > /sbin/reboot << EOF
+#!/bin/sh
+# This file is intentionally changed by huanghao@yy.com
+/bin/kill 1
+EOF
+ln -s /sbin/reboot /sbin/shutdown
+cat > /sbin/init << EOF
+#!/bin/sh
+# This file is intentionally changed by huanghao@yy.com
+>&2 echo init is disabled intentionally.
+EOF
+ln -s /sbin/init /sbin/telinit
+chmod +x /sbin/init /sbin/telinit /sbin/reboot /sbin/shutdown
