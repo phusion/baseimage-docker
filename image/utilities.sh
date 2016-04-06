4da318f05106169c23a83c96f90829545ee0fd64
Homer Huang <aclisp@gmail.com>
2016-04-06 16:28:27 +0800

    Add reboot message to /dev/termination-log

--- image/utilities.sh
+++ image/utilities.sh
@@ -15,6 +15,7 @@ rm -f /sbin/init /sbin/telinit /sbin/reboot /sbin/shutdown
 cat > /sbin/reboot << EOF
 #!/bin/sh
 # This file is intentionally changed by huanghao@yy.com
+/bin/echo -e "\n*** reboot was called from inside container" >> /dev/termination-log
 /bin/kill 1
 EOF
 ln -s /sbin/reboot /sbin/shutdown
