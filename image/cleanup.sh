d9e9717491f8a2e0273675143fc7d8c0c3097172
davwilliams <dwilliams@williamsgarcia.com>
2016-06-06 21:53:16 -0400

    Add additional cleanup steps to further reduce image size

--- image/cleanup.sh
+++ image/cleanup.sh
@@ -6,6 +6,8 @@ set -x
 apt-get clean
 rm -rf /bd_build
 rm -rf /tmp/* /var/tmp/*
+rm -f /var/cache/apt/archives/*.deb
+rm -f /var/cache/apt/*cache.bin
 rm -rf /var/lib/apt/lists/*
 rm -f /etc/dpkg/dpkg.cfg.d/02apt-speedup
 
