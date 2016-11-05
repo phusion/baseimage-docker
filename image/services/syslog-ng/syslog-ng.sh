14ffa738ed7aa343e0460fb682c287b909ca24b6
Javier Sixto <javier.sixto@ulex.fr>
2016-11-05 17:52:02 +0100

    runit use /etc/service and /etc/sv directory To enable a service : ln -s /etc/sv/foo /etc/service To disable : rm /etc/service/foo
    http://smarden.org/runit/faq.html#remove
    http://smarden.org/runit/faq.html#tell

--- image/services/syslog-ng/syslog-ng.sh
+++ image/services/syslog-ng/syslog-ng.sh
@@ -7,17 +7,17 @@ SYSLOG_NG_BUILD_PATH=/bd_build/services/syslog-ng
 
 ## Install a syslog daemon.
 $minimal_apt_get_install syslog-ng-core
-mkdir /etc/service/syslog-ng
-cp $SYSLOG_NG_BUILD_PATH/syslog-ng.runit /etc/service/syslog-ng/run
 mkdir -p /var/lib/syslog-ng
 cp $SYSLOG_NG_BUILD_PATH/syslog_ng_default /etc/default/syslog-ng
 touch /var/log/syslog
 chmod u=rw,g=r,o= /var/log/syslog
 cp $SYSLOG_NG_BUILD_PATH/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
 
-## Install syslog to "docker logs" forwarder.
-mkdir /etc/service/syslog-forwarder
-cp $SYSLOG_NG_BUILD_PATH/syslog-forwarder.runit /etc/service/syslog-forwarder/run
+## Enable runit for syslog-ng and  "docker logs" forwarder
+for SV in syslog-ng syslog-forwarder; do 
+  install -D -m 755 $SYSLOG_NG_BUILD_PATH/$SV.runit /etc/sv/$SV/run
+  ln -s /etc/sv/$SV /etc/service
+done
 
 ## Install logrotate.
 $minimal_apt_get_install logrotate
