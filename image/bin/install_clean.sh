19edcc09539668c73a35e77e0edc41dc259b542d
Steve Kamerman <stevekamerman@gmail.com>
2017-10-17 16:39:19 -0400

    Added `--no-install-recommends` by default

--- image/bin/install_clean.sh
+++ image/bin/install_clean.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash -e
 #  Apt installer helper for Docker images
 
-set -e
+ARGS="$*"
+NO_RECOMMENDS="--no-install-recommends"
+RECOMMENDS="--install-recommends"
+if [[ $ARGS =~ "$RECOMMENDS" ]]; then
+    NO_RECOMMENDS=""
+    ARGS=$(sed "s/$RECOMMENDS//g" <<<"$ARGS")
+fi
 
-echo "Installing $*"
-apt-get -q update && apt-get -qy install $* \
+echo "Installing $ARGS"
+
+apt-get -q update && apt-get -qy install $NO_RECOMMENDS $ARGS \
     && apt-get -qy autoremove \
     && apt-get clean \
     && rm -r /var/lib/apt/lists/*
