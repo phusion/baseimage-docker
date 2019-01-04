d0b150c5ef41b9169181374282464d1f69bc01fd
Panos Mavrogiorgos <pmav99@gmail.com>
2019-01-04 08:01:33 +0200

    Remove python bytecode

--- image/cleanup.sh
+++ image/cleanup.sh
@@ -8,4 +8,8 @@ find /bd_build/ -not \( -name 'bd_build' -or -name 'buildconfig' -or -name 'clea
 rm -rf /tmp/* /var/tmp/*
 rm -rf /var/lib/apt/lists/*
 
+# clean up python bytecode
+find / -name *.pyc -delete
+find / -name *__pycache__* -delete
+
 rm -f /etc/ssh/ssh_host_*
