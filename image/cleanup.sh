9d29b23db225966714f48258eff3dafa8586fbd7
Matyas Markovics <matyas@scrapinghub.com>
2019-03-13 09:59:33 +0100

    exclude mounts (e.g.: /proc) from cleanup

--- image/cleanup.sh
+++ image/cleanup.sh
@@ -9,7 +9,7 @@ rm -rf /tmp/* /var/tmp/*
 rm -rf /var/lib/apt/lists/*
 
 # clean up python bytecode
-find / -name *.pyc -delete
-find / -name *__pycache__* -delete
+find / -mount -name *.pyc -delete
+find / -mount -name *__pycache__* -delete
 
 rm -f /etc/ssh/ssh_host_*
